ccfed7bf900e52a9aaad19fcecf25845e98f7de9
liujikuan <liujikuan.job@gmail.com>
2025-05-17 22:40:16 +1000

    just a test 3

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, world! This is a Flask app running in a Docker container. welcome"
+    return "Hello, world! This is a Flask app running in a Docker container."
 
 
 @app.route("/healthz")
