2ba82a875396a0edaead60ec43d0b9a79d3cc5ce
liujikuan <liujikuan.job@gmail.com>
2025-05-19 12:20:48 +1000

    a broken code

--- app/app.py
+++ app/app.py
@@ -1,24 +1,10 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello():
-    return "Hello, world! This is a Flask app running in a Docker container."
-
-
-@app.route("/healthz")
-def health():
-    return "OK", 500
-
-
-@app.route("/greet")
-def greet():
-    name = request.args.get("name", "Guest")
-    safe_name = ''.join(c for c in name if c.isalnum() or c.isspace())
-    return f"Hello, {safe_name}!"
-
+    return "Hello, world!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
